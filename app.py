cce030ad729f08d87a06ed9dcd904ab83fc7e84d
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2020-02-21 12:04:51 +0530

    Added chrome options

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def predict():
     url = [str(x) for x in request.form.values()]
     url=url[0]
 
-    options = webdriver.ChromeOptions()
+    chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
