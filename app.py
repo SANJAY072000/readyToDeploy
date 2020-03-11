693ad75423c59e97da8124a6b2867843023b3832
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2020-03-11 20:23:19 +0530

    Updated scrolling

--- app.py
+++ app.py
@@ -14,23 +14,29 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
+
     url = [str(x) for x in request.form.values()]
     url=url[0]
 
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    # for heroku deployment
+    if request.url_root == 'https://extracturl.herokuapp.com/':
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),
+         chrome_options=chrome_options)
 
 
-    # options = webdriver.ChromeOptions()
-    # options.add_argument('--ignore-certificate-errors')
-    # options.add_argument("--test-type")
-    # options.add_argument("--headless")
-    # options.binary_location = "/usr/bin/chromium"
-    # driver = webdriver.Chrome("./chromedriver_win32/chromedriver.exe")
+    # for local deployment
+    else:
+        options = webdriver.ChromeOptions()
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument("--test-type")
+        options.add_argument("--headless")
+        options.binary_location = "/usr/bin/chromium"
+        driver = webdriver.Chrome("./chromedriver_win32/chromedriver.exe")
 
     lst2,allemnts=list(),list()
 
@@ -40,29 +46,19 @@ def predict():
         pass
 
 
-    try:
-        driver.refresh()
-    except TimeoutException:
-        pass
-
-
-    time.sleep(1)
-
-
     allemnts = driver.find_elements()
 
 
-    # # JS pages take time to load and sometimes after we scroll down the page
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);"
-                          "var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+    # JS pages take time to load and sometimes new components load after we scroll down the page
+    # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);"
+    #                       "var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+    #
+    # allemnts += driver.find_elements()
 
-    time.sleep(1)
-    allemnts += driver.find_elements()
-
-    # for i in range(3):
-    #     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    #     time.sleep(2)
-    #     allemnts += driver.find_elements()
+    for i in range(6):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(0.5)
+        allemnts += driver.find_elements()
 
 
     # lst1 is the list of all urls
@@ -93,7 +89,6 @@ def predict():
                 pass
 
 
-    time.sleep(1)
     driver.close()
     data = list(set(lst1))
 
